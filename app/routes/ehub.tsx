c53f321d6723fee7c326657c976f4d9069603d79
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-09 17:00:58 +0300

    Update ehub.tsx

--- app/routes/ehub.tsx
+++ app/routes/ehub.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 import { useNavigate } from "react-router";
-import EhubNineDotsDropdown from "../components/EhubNineDotsDropdown";
+import EhubNineDotsMenu from "../components/EhubNineDotsMenu";
 
 export default function EhubPage() {
   const navigate = useNavigate();
