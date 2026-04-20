7fd608c68ffae8f79ce1e9555cb063d07ccd1869
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-20 20:25:44 +0300

    Update password.tsx

--- app/components/accounts/password.tsx
+++ app/components/accounts/password.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Layout from "@/components/Layout";
+import Layout from "./components/Layout";
 import { Eye, EyeOff } from "lucide-react";
 
 export default function Password() {
