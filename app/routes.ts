9d54a9c016e4378f022878f3fdc788029bf4c733
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-21 13:40:02 +0300

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -18,4 +18,8 @@ export default [
   route("/learning-login", "routes/learning-login.tsx"),
   route("/signup", "routes/signup.tsx"),
   route("/learning-resources-page", "routes/learning-resources-page.tsx"),
+  route("/account-paymrnts", "routes/account-payments.tsx"),
+  route("/account-password", "routes/account-password.tsx"),
+  route("/account-paymentFAQs", "routes/account-paymentFAQs.tsx"),
+  route("/account-hubattendance", "routes/account-hubattendance.tsx"),
 ] satisfies RouteConfig;
