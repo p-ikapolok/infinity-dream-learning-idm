ce6a07f866120b3f039b943ec7c74866a774e0eb
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-21 13:29:47 +0300

    Update layout.tsx

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,12 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex-1">{children}</main>
+      <main className="flex-1">
+        {children}
+      </main>
 
       <Footer />
     </div>
