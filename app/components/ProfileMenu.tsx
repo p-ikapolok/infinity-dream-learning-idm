a15ab373fa2a5266a9826a5512852dc801d2d175
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 00:28:32 +0300

    Update ProfileMenu.tsx

--- app/components/ProfileMenu.tsx
+++ app/components/ProfileMenu.tsx
@@ -38,7 +38,7 @@ export default function ProfileMenu({ menuOpen }: ProfileMenuProps) {
 
       {/* View profile */}
       <button
-        onClick={() => navigate("/account-payments")}
+        onClick={() => navigate("/settings-personal-information")}
         className="flex items-center space-x-2 w-full text-left text-gray-700 hover:text-blue-600"
       >
         <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="1.8" viewBox="0 0 24 24">
@@ -50,7 +50,7 @@ export default function ProfileMenu({ menuOpen }: ProfileMenuProps) {
 
       {/* Settings */}
       <button
-        onClick={() => navigate("/settings-personal-information")}
+        onClick={() => navigate("/account-paymwnt")}
         className="flex items-center space-x-2 w-full text-left text-gray-700 hover:text-blue-600"
       >
         <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="1.8" viewBox="0 0 24 24">
