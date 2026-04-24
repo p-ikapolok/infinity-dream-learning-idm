2b203c1aedfd5543764c3e58f1151f08e70012dc
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-24 17:42:06 +0300

    Update AboutModal.tsx

--- app/components/AboutModal.tsx
+++ app/components/AboutModal.tsx
@@ -1,8 +1,13 @@
 export default function AboutModal({ onClose }: any) {
   return (
-    <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50">
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
-      <div className="bg-white w-[650px] rounded-xl shadow-lg p-6">
+    <div
+  className="fixed inset-0 bg-black/30 flex items-center justify-center z-50"
+  onClick={onClose}
+>
+  <div
+    className="bg-white w-[650px] rounded-xl shadow-lg p-6"
+    onClick={(e) => e.stopPropagation()}
+  >
 
         {/* Header */}
         <div className="flex justify-between items-start mb-4">
