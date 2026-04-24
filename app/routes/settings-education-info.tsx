a44629ec794967771c27024d6ea02eb08f5cfc22
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 00:20:21 +0300

    Update settings-education-info.tsx

--- app/routes/settings-education-info.tsx
+++ app/routes/settings-education-info.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import Layout from "../components/layout";
 import { useState, useEffect } from "react";
+import BasicInfoModal from "../components/BasicInfoModal";
 
 export default function EducationInfo() {
     const profile = {
@@ -8,6 +9,8 @@ export default function EducationInfo() {
   location: "Kenya",
   status: "Open to Opportunities"
 };
+
+const [openBasicInfo, setOpenBasicInfo] = useState(false);
 
 // STATE (declare first)
 const [coverImage, setCoverImage] = useState<string | null>(null);
@@ -140,10 +143,10 @@ return (
         Copy link
       </button>
 
-      <button
-        onClick={onEdit}
-        className="w-10 h-10 rounded-lg bg-white text-blue-700 flex items-center justify-center shadow border border-gray-200 hover:bg-gray-50"
-      >
+            <button
+  onClick={() => setOpenBasicInfo(true)}
+  className="w-10 h-10 rounded-lg bg-white text-blue-700 flex items-center justify-center shadow border border-gray-200 hover:bg-gray-50"
+>
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
           <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z"/>
           <path d="M20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/>
@@ -311,6 +314,9 @@ return (
     </div>
   </div>
 </div>
+{openBasicInfo && (
+  <BasicInfoModal onClose={() => setOpenBasicInfo(false)} />
+)}
     </Layout>
   );
 }
