796073c56c125a307348a6cc9d676d7bc41bc170
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 11:56:42 +0300

    Update SkillsModal.tsx

--- app/components/SkillsModal.tsx
+++ app/components/SkillsModal.tsx
@@ -6,7 +6,10 @@ const CloseIcon = () => (
 
 export default function SkillsModal() {
   return (
-    <div className="fixed inset-0 bg-black/30 flex items-center justify-center">
+    <div
+  className="fixed inset-0 bg-black/30 flex items-center justify-center"
+  onClick={onClose}
+>
       <div className="w-[600px] bg-white rounded-[16px] px-[24px] py-[20px] shadow-xl relative">
 
         <button className="absolute right-[16px] top-[16px] text-gray-400">
