967617103006e0a8e67e2c584ae397dc3c81f1e0
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 11:45:03 +0300

    Update LinkedInModal.tsx

--- app/components/LinkedInModal.tsx
+++ app/components/LinkedInModal.tsx
@@ -4,9 +4,12 @@ const CloseIcon = () => (
   </svg>
 );
 
-export default function LinkedInModal() {
+export default function LinkedInModal({ onClose }) {
   return (
-    <div className="fixed inset-0 bg-black/30 flex items-center justify-center">
+    <div
+  className="fixed inset-0 bg-black/30 flex items-center justify-center"
+  onClick={onClose}
+>
       <div className="w-[600px] bg-white rounded-[16px] px-[24px] py-[20px] shadow-xl relative">
 
         <button className="absolute right-[16px] top-[16px] text-gray-400">
