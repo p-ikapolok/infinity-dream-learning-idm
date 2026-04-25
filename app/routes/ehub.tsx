af2aefb9521252febb60a9b5031000ff17082b35
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 04:07:06 +0300

    Update ehub.tsx

--- app/routes/ehub.tsx
+++ app/routes/ehub.tsx
@@ -95,9 +95,12 @@ export default function EhubPage() {
   </button>
 
   {/* Profile dropdown menu connected */}
+  {profileOpen && (
   <div className="absolute right-0 mt-3 z-50">
-  <ProfileMenu menuOpen={profileOpen} />
-</div>
+    <ProfileMenu menuOpen={profileOpen} />
+  </div>
+)}
+   </div>
       </div>
       </header>
 
