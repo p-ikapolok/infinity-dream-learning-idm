bd76fb56b99c6af615843fef1eca8da6e86ece8a
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-25 03:58:30 +0300

    Update support.tsx

--- app/routes/support.tsx
+++ app/routes/support.tsx
@@ -99,7 +99,6 @@ export default function SupportFullPage() {
   <div className="absolute right-0 mt-3 z-50">
   <ProfileMenu menuOpen={profileOpen} />
 </div>
-</div>
         </div>
       </header>
 
