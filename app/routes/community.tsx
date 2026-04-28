1df130afd87eaa47fe3899761d12296c6c32c26f
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-28 21:47:16 +0300

    Update community.tsx

--- app/routes/community.tsx
+++ app/routes/community.tsx
@@ -539,23 +539,16 @@ const CommentIcon = () => (
       </svg>
     </button>
 
-    {/* PROFILE ICON */}
-<div
-  onClick={() => setProfileOpen(!profileOpen)}
-  className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center hover:bg-gray-300 transition shadow-sm cursor-pointer"
->
-  <svg
-    viewBox="0 0 24 24"
-    className="w-6 h-6 text-gray-700"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="1.8"
-  >
-    <circle cx="12" cy="8" r="4" />
-    <path d="M4 20a8 8 0 0116 0" />
-  </svg>
-</div>
-
+    {/* three dots */}  
+ <button className="text-gray-500 hover:text-gray-700">     
+<svg className="w-6 h-6"       
+fill="currentColor"       
+viewBox="0 0 24 24">       
+<circle cx="6" cy="12" r="1.5" />       
+<circle cx="12" cy="12" r="1.5" />      
+<circle cx="18" cy="12" r="1.5" />    
+ </svg>   
+</button>
   </div>
 </div>
 
