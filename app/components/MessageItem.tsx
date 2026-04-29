4ad3b3d1a928c307acc1e5b3281100b27447d210
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-04-29 20:02:15 +0300

    Update MessageItem.tsx

--- app/components/MessageItem.tsx
+++ app/components/MessageItem.tsx
@@ -6,7 +6,7 @@ export default function MessageItem({ msg }) {
   return (
     <div
       onClick={() =>
-        navigate(`/CommunityDirectMessageChat/${msg.id}`)
+        navigate(`/message/${msg.id}`)
       }
       className="cursor-pointer hover:bg-gray-100 px-3 py-2 rounded-lg transition"
     >
