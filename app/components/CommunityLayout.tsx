05f6c253cb0f11ac88d8c0b3fdc9d07d1b1510e4
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-02 22:05:34 +0300

    Update CommunityLayout.tsx

--- app/components/CommunityLayout.tsx
+++ app/components/CommunityLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import Header from "./feader";
+import Header from "./header";
 import Banner from "./Banner";
 import CommunityHeader from "./CommunityHeader";
 import Sidebar from "./CommunitySidebar";
