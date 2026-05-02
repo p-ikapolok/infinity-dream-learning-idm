e0555227f3001479cf1e561ffb4ecbca746f7052
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-02 19:05:38 +0300

    Update CommunityLayout.tsx

--- app/components/CommunityLayout.tsx
+++ app/components/CommunityLayout.tsx
@@ -1,1 +1,52 @@
-.
+import { ReactNode } from "react";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import CommunityHeader from "../components/CommunityHeader";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+
+type Props = {
+  children: ReactNode;
+  sidebarOpen: boolean;
+  setSidebarOpen: (value: boolean) => void;
+};
+
+export default function CommunityLayout({
+  children,
+  sidebarOpen,
+  setSidebarOpen,
+}: Props) {
+  return (
+    <div className="bg-gray-50 min-h-screen flex flex-col">
+
+      {/* HEADER 1 */}
+      <Header />
+
+      {/* BANNER */}
+      <Banner />
+
+      {/* HEADER 2 (Community Header) */}
+      <CommunityHeader
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+
+      {/* MAIN AREA */}
+      <div className="flex flex-1 items-stretch">
+
+        {/* SIDEBAR */}
+        {sidebarOpen && <Sidebar />}
+
+        {/* PAGE CONTENT */}
+        <div className="flex-1 flex flex-col">
+          {children}
+        </div>
+
+      </div>
+
+      {/* FOOTER */}
+      <Footer />
+
+    </div>
+  );
+}
