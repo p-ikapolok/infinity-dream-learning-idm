ccd3a105b4e068cdea66fc921fd7354796c3aad6
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-03 10:55:02 +0300

    Update CommunityLayout.tsx

--- app/components/CommunityLayout.tsx
+++ app/components/CommunityLayout.tsx
@@ -37,7 +37,7 @@ return (
   </aside>
 
   {/* RIGHT SIDE */}
-  <div className="flex-1 flex flex-col">
+  <div className="flex-1 flex flex-col min-h-0">
 
     <CommunityHeader
       sidebarOpen={sidebarOpen}
