27d906492816d0ae678a706a29f135fec99961e6
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-05 18:48:02 +0300

    Update aipage.tsx

--- app/routes/aipage.tsx
+++ app/routes/aipage.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/home";
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router";
 import PublicNavbar from "../components/PublicNavbar";
+import FloatingFinley from "../components/FloatingFinley";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -60,6 +61,7 @@ export default function AITechPage() {
 
   return (
     <div className={darkMode ? "dark min-h-screen" : "min-h-screen"}>
+       <FloatingFinley />
       <div className="bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100 transition-colors duration-500 font-sans pb-10">
         
         {/* HERO */}
