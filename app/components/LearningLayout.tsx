c787355aeb646ef1968497806c75f341dd2508a1
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-14 00:54:00 +0300

    Update LearningLayout.tsx

--- app/components/LearningLayout.tsx
+++ app/components/LearningLayout.tsx
@@ -12,9 +12,7 @@ const [collapsed, setCollapsed] = useState(false);
     <div className="flex bg-gray-100 min-h-screen">
 
       {/* SIDEBAR */}
-      <Sidebar
-        collapsed={collapsed}
-      />
+      <Sidebar collapsed={collapsed} />
 
       {/* MAIN AREA */}
       <div className="flex-1 flex flex-col min-h-screen">
