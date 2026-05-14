48b0b9f1ff2ba8a11f7a241dc6113ab8fb57b4b5
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-14 16:03:42 +0300

    Update LearningFooter.tsx

--- app/components/LearningFooter.tsx
+++ app/components/LearningFooter.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-      <footer className="text-center text-xs text-gray-400 py-2">
+      <footer className="shrink-0 text-center text-xs text-gray-400 py-2 border-t bg-white">
     Copyright © 2026 Infinity
   </footer>
   );
