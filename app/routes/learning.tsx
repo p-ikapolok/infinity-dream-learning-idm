9ddee734bd26cdbf03111a5c9607ecd9cd892dac
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-05-15 07:15:45 +0300

    Update learning.tsx

--- app/routes/learning.tsx
+++ app/routes/learning.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LearningLayout from "../components/LearningLayout";
 
